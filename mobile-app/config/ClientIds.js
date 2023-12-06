84794f1b1121f4a4768a3349ff9bd10ae7186163
dawodahmadi <118851483+dawodahmadi@users.noreply.github.com>
2023-12-06 01:30:20 -0500

    solved built errors

---
+++ mobile-app/config/ClientIds.js
@@ -0,0 +1,8 @@
+const ClientIds = {
+    googleClientId: "YOUR_GOOGLE_CLIENT_ID",
+    facebookClientId: "YOUR_FACEBOOK_CLIENT_ID",
+    // ... other client IDs
+  };
+  
+  export default ClientIds;
+  
